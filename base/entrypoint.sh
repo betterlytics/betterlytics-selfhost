b8953417c5806163706d1e4c663f311d64227684
Lindharden <jeppe@kripet.dk>
2026-09-15 20:52:32 +0200

    Updated env (#7)

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -11,8 +11,7 @@ if [ -n "$SECRET_BASE" ]; then
     export CLICKHOUSE_DASHBOARD_PASSWORD=$(derive_secret "clickhouse-dashboard" 32)
     export POSTGRES_PASSWORD=$(derive_secret "postgres" 32)
     export POSTGRES_SITECONFIG_RO_PASSWORD=$(derive_secret "postgres-siteconfig-ro" 32)
-    export NEXTAUTH_SECRET=$(derive_secret "nextauth" 64)
-    export TOTP_SECRET_ENCRYPTION_KEY=$(derive_secret "totp-encryption" 32)
+    export AUTH_SECRET=$(derive_secret "auth" 64)
 
     export POSTGRES_URL="postgresql://user:${POSTGRES_PASSWORD}@postgres:5432/dashboard?schema=public"
     export SITE_CONFIG_DATABASE_URL="postgresql://siteconfig_ro:${POSTGRES_SITECONFIG_RO_PASSWORD}@postgres:5432/dashboard"
